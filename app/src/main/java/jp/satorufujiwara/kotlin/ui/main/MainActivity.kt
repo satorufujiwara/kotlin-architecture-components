0a719a8381fa88fd79b8a7935e7fad507563cc3d
fujiwara-satoru <fujiwara_satoru@cyberagent.co.jp>
2017-06-08 15:40:29 +0900

    Remove log.

--- app/src/main/java/jp/satorufujiwara/kotlin/ui/main/MainActivity.kt
+++ app/src/main/java/jp/satorufujiwara/kotlin/ui/main/MainActivity.kt
@@ -2,7 +2,6 @@ package jp.satorufujiwara.kotlin.ui.main
 
 import android.arch.lifecycle.LifecycleRegistryOwner
 import android.arch.lifecycle.ViewModelProvider
-import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
@@ -10,14 +9,12 @@ import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
 import jp.satorufujiwara.kotlin.R
 import jp.satorufujiwara.kotlin.util.ext.setContentFragment
-import timber.log.Timber
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), LifecycleRegistryOwner, HasSupportFragmentInjector {
 
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
     @Inject lateinit var androidInjector: DispatchingAndroidInjector<Fragment>
-    private val viewModel by lazy { ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java) }
     private val lifecycleRegistry by lazy { android.arch.lifecycle.LifecycleRegistry(this) }
 
     override fun getLifecycle() = lifecycleRegistry
@@ -27,7 +24,6 @@ class MainActivity : AppCompatActivity(), LifecycleRegistryOwner, HasSupportFrag
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        Timber.d("ViewModel = $viewModel")
         setContentFragment(R.id.containerLayout) { MainFragment.newInstance() }
     }
 }
