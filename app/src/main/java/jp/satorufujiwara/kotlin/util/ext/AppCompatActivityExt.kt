9c7c1c36c0329b81074b52d474294222728c5702
fujiwara-satoru <fujiwara_satoru@cyberagent.co.jp>
2017-06-09 19:20:50 +0900

    Use it.

--- app/src/main/java/jp/satorufujiwara/kotlin/util/ext/AppCompatActivityExt.kt
+++ app/src/main/java/jp/satorufujiwara/kotlin/util/ext/AppCompatActivityExt.kt
@@ -6,6 +6,6 @@ import android.support.v4.app.FragmentActivity
 inline fun FragmentActivity.setContentFragment(containerViewId: Int, f: () -> Fragment): Fragment? {
   val manager = supportFragmentManager
   val fragment = manager?.findFragmentById(containerViewId)
-  fragment?.let { return fragment }
+  fragment?.let { return it }
   return f().apply { manager?.beginTransaction()?.add(containerViewId, this)?.commit() }
 }
